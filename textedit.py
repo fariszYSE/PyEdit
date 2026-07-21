d572a9796ebc61952d8662df78609b69d68e2979
fariszYSE <faristamer@duck.com>
2026-07-21 17:56:35 +0300

    Modify textedit.py to handle file reading and data appending
    Append data to textfile and read from opened_file.

--- textedit.py
+++ textedit.py
@@ -2,3 +2,11 @@ import os
 import Tkinter as tk
 import tkFileDialog
 import tkMessageBox
+
+textfile =[""]
+textfile.append(data)
+
+with open("opened_file","rb"):
+  data = f.read()
+print(f"data is: {data} \n")
+print(f"textfile data is {textfile} \n")
