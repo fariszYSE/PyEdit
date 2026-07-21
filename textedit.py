a896fb6736424c44bcb208847572ac0232af62a8
fariszYSE <faristamer@duck.com>
2026-07-21 18:24:21 +0300

    Update textedit.py

--- textedit.py
+++ textedit.py
@@ -1,12 +1,24 @@
 import os
-import Tkinter as tk
-import tkFileDialog
-import tkMessageBox
-
-textfile =[""]
-textfile.append(data)
-
-with open("opened_file","rb"):
+#import Tkinter as tk
+#import tkFileDialog
+#import tkMessageBox
+file =input("write the absolute path to the file you wish to edit in absolutely no double or single quotes:\n")
+textfile =""
+os.system("")
+with open("file","r") as f:
   data = f.read()
+  textfile += data
 print(f"data is: {data} \n")
 print(f"textfile data is {textfile} \n")
+def update():
+    global textfile
+    with open(file,"r") as f:
+      data = f.read()
+      textfile += data
+      textfile += stdin
+    print(f"new textfile is: {textfile}")
+    with open(file,"w") as f:
+       f.write(textfile)
+while True:
+  stdin = input()
+  update()
