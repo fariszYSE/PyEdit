6ac68a5d2c0187cfb141251ef20e5138bb3a2350
fariszYSE <faristamer@duck.com>
2026-09-16 20:32:50 +0300

    Implement logging and debug file in main.py
    Added logging functionality and debug file creation.

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 #the starting point
-from textedit import loop, on_start
-print("running textedit..")
-on_start()
+import curses
+import logging
+with open("debug.log", "w") as f:
+    f.write("")
+logging.basicConfig(
+    filename="debug.log",
+    level=logging.DEBUG
+)
+from textedit import loop, on_start,file
+logging.info("running textedit..\n")
+logging.info(f"file before value:\n{file}\n")
+curses.wrapper(on_start)
+#on_start()
 def gui():
     pass
-print("this does nothing yet! but it will i swear") #cappp
+print("log written in 'debug.log' file")
+
+
+
