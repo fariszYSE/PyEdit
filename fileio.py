26e3e660680d16f6ba99f164c46a8b36253cf6ca
fariszYSE <faristamer@duck.com>
2026-09-16 20:32:32 +0300

    Implement logging for file operations
    Added logging to track file read/write operations.

--- fileio.py
+++ fileio.py
@@ -1,12 +1,21 @@
 import textedit
 import os
+import logging
+
+logging.basicConfig(
+    filename="debug.log",
+    level=logging.DEBUG
+)
 def readfile():
     try:
+        logging.info("file read")
         with open(textedit.file_name,"r") as f:
             textedit.file = f.read()
     except:
+        logging.warning("attempted to read, file not found so file written instead")
         writefile()
 def writefile():
     with open(textedit.file_name,"w") as f:
+        logging.info(f"wrote data to file\nnew file data is:\n{textedit.file}\n")
         textedit.testgrid = "".join(textedit.testgrid)
-        f.write(textedit.testgrid)
+        f.write(textedit.file)
